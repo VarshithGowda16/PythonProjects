8018de93b2a96306fb812895112922ab30461d3d
VarshithGowda16 <varshith.p@s.amity.edu>
2025-05-11 22:38:17 +0530

    this program adds two numbers

--- new2.c
+++ new2.c
@@ -2,6 +2,16 @@
 
 int main()
 {
-    printf("welcome varshith");
+    printf("this program adds to numbers.");
+    int a;
+    int b;
+    int result;
+    printf("enter a: ");
+    scanf("%d",&a);
+    printf("enter b: ");
+    scanf("%d",&b);
+
+    result = a + b;
+    printf("\nsum of a and b is: ",result);
     return 0;
 }
